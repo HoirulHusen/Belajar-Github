0839f46454e764f46eeaa222c24d7bcb9348d7dd
idCoder <37359862+HoirulHusen@users.noreply.github.com>
2018-04-13 10:33:20 +0700

    Game Suwit Jawa V2.0
    menggunakan HTML, CSS, dan Javascript.
    
    Refrensi from Shandika Galih
    "web programming unpas"

---
+++ game Suwit Jawa V2.0/css/script.js
@@ -0,0 +1,106 @@
+function getPilihanComputer() {
+	const comp = Math.random();
+	if( comp < 0.34 ) return 'gajah';
+	if( comp >= 0.34 && comp < 0.67 ) return 'orang'; return 'semut';
+}
+
+function getHasil(comp, player) {
+	if( player == comp ) return 'OH ANDA SERI!';
+	if( player == 'gajah' ) return ( comp == 'orang' ) ? 'OK ANDA MENANG!' : 'KALAH!';
+	if( player == 'orang' ) return ( comp == 'gajah' ) ? 'UPS ANDA KALAH!' : 'MENANG!';
+	if( player == 'orang' ) return ( comp == 'orang' ) ? 'UPS ANDA KALAH!' : 'OK ANDA MENANG!';
+}
+
+
+// const pGajah = document.querySelector('.gajah');
+// pGajah.addEventListener('click', function() {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pGajah.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// })
+
+// const pOrang = document.querySelector('.orang');
+// pOrang.addEventListener('click', function() {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pOrang.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// })
+
+// const pSemut = document.querySelector('.semut');
+// pSemut.addEventListener('click', function() {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pSemut.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// })
+
+
+function putar() {
+	const imgComputer = document.querySelector('.img-komputer');
+	const gambar = ['gajah', 'semut', 'orang'];
+	let i = 0;
+	const waktuMulai = new Date().getTime();
+	setInterval(function() {
+		if( new date().getTime() - waktuMulai > 1000 ) {
+			clearInterval;
+			return;
+		}
+		imgComputer.setAttribute('src', 'img/' + gambar[i++] + '.png');
+		if(i == gambar.length) i = 0;
+
+	}, 100)
+}
+
+
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function(pil) {
+	pil.addEventListener('click', function() {
+		const pilihanComputer = getPilihanComputer();
+		const pilihanPlayer = pil.className;
+		const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+		putar();
+
+		setTimeout(function() {
+			const imgComputer = document.querySelector('.img-komputer');
+			imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+			const info = document.querySelector('.info');
+			info.innerHTML = hasil;
+		}, 500);
+		
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
